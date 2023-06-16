ea0944dcf8e545a00f3681b1c3a5cc1d4fd6d762
Aleksandra Roguszewska <ola.roguszewska@gmail.com>
2023-06-16 10:03:11 +0200

    Add basic structure

--- plant-lover/src/App.jsx
+++ plant-lover/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import { Toaster } from "react-hot-toast";
+import LandingPage from "./components/LandingPage/LandingPage";
+import styles from "./App.module.css";
+import Login from "./components/Auth/Login/Login";
+import Register from "./components/Auth/Register/Register";
+import Plants from "./components/Plants/Plants";
+import Tasks from "./components/Tasks/Tasks";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={styles.app}>
+      <div className={styles.navbar}>
+        <Navbar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className={styles.page_content}>
+        <Toaster />
+        <Routes>
+          {/* Publiczne ściezki */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* Ściezki prywatne */}
+          <Route path="/plants" element={<Plants />} />
+          <Route path="/tasks" element={<Tasks />} />
+        </Routes>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
