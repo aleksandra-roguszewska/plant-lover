093bc3866712614f4144b7add5c0b50195606a86
Aleksandra Roguszewska <ola.roguszewska@gmail.com>
2023-07-18 13:16:36 +0200

    feat: set up firebase

--- plant-lover/src/components/Layout/Layout.tsx
+++ plant-lover/src/components/Layout/Layout.tsx
@@ -1,5 +1,23 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+import { Toaster } from "react-hot-toast";
+
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Toaster />
+        <Outlet />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
 };
 
 export default Layout;
