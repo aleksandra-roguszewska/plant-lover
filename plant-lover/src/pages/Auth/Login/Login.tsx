cb0ea25cd3fb83accc4cbeae43238f44d5e0f769
Aleksandra Roguszewska <ola.roguszewska@gmail.com>
2023-08-17 23:09:55 +0200

    Add login validation with react-hook-form in login

--- plant-lover/src/pages/Auth/Login/Login.tsx
+++ plant-lover/src/pages/Auth/Login/Login.tsx
@@ -1,50 +1,52 @@
-import { useState } from 'react'
-import { Navigate, useLocation, useNavigate } from 'react-router'
-import useAuth from '../../../context/AuthContext'
-import { toast } from 'react-hot-toast'
-import { firebaseErrors } from '../../../utils/firebaseErrors'
-import { FaEye, FaEyeSlash } from 'react-icons/fa'
-import { AuthButtonPrimary } from '../../../components/UI/buttons/AuthButtonPrimary.styled'
-import { AuthButtonSecondary } from '../../../components/UI/buttons/AuthButtonSecondary.styled'
-import { H1Forms } from '../../../components/UI/text/H1.style'
-import { StyledForm } from '../../../components/UI/forms/Form.styled'
-import { StyledInput } from '../../../components/UI/forms/Input.styled'
-import { Flex } from '../../../components/UI/Flex.styled'
-import { StyledPasswordInputCotainer } from '../../../components/UI/forms/PasswordInput.styled'
-import { useForm } from 'react-hook-form'
+import { useState } from "react";
+import { Navigate, useLocation, useNavigate } from "react-router";
+import useAuth from "../../../context/AuthContext";
+import { toast } from "react-hot-toast";
+import { firebaseErrors } from "../../../utils/firebaseErrors";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { AuthButtonPrimary } from "../../../components/UI/buttons/AuthButtonPrimary.styled";
+import { AuthButtonSecondary } from "../../../components/UI/buttons/AuthButtonSecondary.styled";
+import { H1Forms } from "../../../components/UI/text/H1.style";
+import { StyledForm } from "../../../components/UI/forms/Form.styled";
+import { StyledInput } from "../../../components/UI/forms/Input.styled";
+import { Flex } from "../../../components/UI/Flex.styled";
+import { StyledPasswordInputCotainer } from "../../../components/UI/forms/PasswordInput.styled";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-  const [showPassword, setShowPassword] = useState(false)
-  const { login, currentUser } = useAuth()
-  const { register, handleSubmit } = useForm<FormData>() //register - api that allows us to register individual inputs into the hook
+  const [showPassword, setShowPassword] = useState(false);
+  const { login, currentUser } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ defaultValues: { email: "", password: "" } }); //register - api that allows us to register individual inputs into the hook
 
-  const location = useLocation()
-  const navigate = useNavigate()
+  const location = useLocation();
+  const navigate = useNavigate();
 
   type FormData = {
-    email: string
-    password: string
-  }
+    email: string;
+    password: string;
+  };
 
   const onSubmit = async (data: FormData) => {
     try {
-      await login(data.email, data.password)
-      console.log('Success')
-      toast.success('Successful login')
-      navigate('/plants')
+      await login(data.email, data.password);
+      toast.success("Successful login");
+      navigate("/plants");
     } catch (error: any) {
       {
         firebaseErrors[error.code]
           ? toast.error(firebaseErrors[error.code])
-          : toast.error('An error occurred. Please try again later. ' + error)
-        console.log(error)
+          : toast.error("An error occurred. Please try again later. " + error);
       }
     }
-  }
+  };
 
   const handleTogglePasswordVisibility = () => {
-    setShowPassword((prevShowPassword) => !prevShowPassword)
-  }
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
 
   return (
     <Flex $alignitems="center" $justifycontent="center" $height="100%">
@@ -54,18 +56,23 @@ const Login = () => {
           <Flex $flexdirection="column">
             <StyledInput
               type="email"
-              {...register('email')}
+              {...register("email", { required: "This field is required" })}
               id="email"
               placeholder="Email"
             />
+            <p>{errors.email?.message}</p>
             <StyledPasswordInputCotainer>
               <StyledInput
-                type={showPassword ? 'text' : 'password'}
-                {...register('password')}
+                type={showPassword ? "text" : "password"}
+                {...register("password", {
+                  required: "This field is required",
+                  minLength: { value: 6, message: "This is required" },
+                })}
                 name="password"
                 id="password"
                 placeholder="Password"
               />
+              <p>{errors.password?.message}</p>
               <button type="button" onClick={handleTogglePasswordVisibility}>
                 {showPassword ? (
                   <FaEye size="1rem" />
@@ -83,7 +90,7 @@ const Login = () => {
           <AuthButtonSecondary
             type="button"
             onClick={() => {
-              navigate('/register')
+              navigate("/register");
             }}
           >
             Register
@@ -95,7 +102,7 @@ const Login = () => {
         <Navigate to="/" />
       )}
     </Flex>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
